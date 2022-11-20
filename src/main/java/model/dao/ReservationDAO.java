35202b7e4b1ad04aec3eb1a57c8ef023f06af63b
chaechae118 <codbs43@naver.com>
2022-11-21 07:52:22 +0900

    Order/Reservation

---
+++ src/main/java/model/dao/ReservationDAO.java
@@ -0,0 +1,32 @@
+package model.dao;
+
+import java.sql.SQLException;
+
+public class ReservationDAO {
+   private JDBCUtil jdbcUtil = null;
+   
+   public ReservationDAO() {
+      jdbcUtil = new JDBCUtil();
+   }
+   
+   //클래스 예약하기
+   public int classReservation(String classId) throws SQLException {
+      String query = "select * from class where maxNum ≠ currenNum";
+      jdbcUtil.setSqlAndParameters(query, new Object[] {classId});
+      
+      try {
+         
+         int result = jdbcUtil.executeUpdate();
+         return result;
+         
+      } catch (Exception ex) {
+         jdbcUtil.rollback();
+         ex.printStackTrace();
+      } finally {
+         jdbcUtil.commit();
+         jdbcUtil.close();
+      }
+      return 0;
+   }
+   
+}
